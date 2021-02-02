8796c507af74b038be89bce846b67d5c7a70b8f7
Lindally <454842352@qq.com>
2021-02-02 17:10:12 +0800

    第一次作业

--- pythoncode/Calculator.py
+++ pythoncode/Calculator.py
@@ -1,7 +1,18 @@
-class Calculator():
+"""
+定义计算器的类
+"""
 
+#被测类
+class Calculator():
+    # 加法计算器
     def add(self, a, b):
         return a + b
 
+    # 除法计算器
     def div(self, a, b):
-        return a / b
+        try:
+            a / b
+        except ZeroDivisionError:
+            print("You can't divided by 0!")
+        else:
+            return a / b
--- testing_calculator/test_cal.py
+++ testing_calculator/test_cal.py
@@ -1,12 +1,49 @@
+import sys
+
+# print(sys.path)
+sys.path.append("..")
+
 from pythoncode.Calculator import Calculator
+import pytest
+import yaml
 
 
+# 测试类
 class TestCalc:
 
-    def test_add(self):
-        cal = Calculator()
-        assert 3 == cal.add(1, 1)
+    # 定义类开始要做的操作：实例化一个计算器
+    def setup_class(self):
+        print("开始计算")
+        self.cal = Calculator()
 
+    def teardown_class(self):
+        print("结束计算")
+
+    # 测试加法计算器
+    # 给加法计算器添加一个标签
+    @pytest.mark.search
+    # 参数化，可以实现生成多条测试用例，即便有测试用例失败了，其他测试用例也可以正常执行，而不会中断执行。
+    @pytest.mark.parametrize("a,b,result",[
+        [1,1,2],[100,200,300],[3,4,8],[200,200,400]
+    ])
+    #获取yaml文件的数值
+    def get_datas(self):
+        with open("./datas/calc.yml") as f:
+            self.datas = yaml.safe_load(f)
+            print("datas")
+            return self.datas
+
+
+
+
+    def test_add(self,a,b,result):
+        print(f"a = {a},b = {b},result = {result}")
+        # cal = Calculator()
+        assert result == self.cal.add(a,b)
+
+    # 测试减法计算器
+    # 给除法计算器添加一个标签
+    @pytest.mark.login
     def test_div(self):
-        cal = Calculator()
-        assert 2 == cal.div(4, 2)
+        # cal = Calculator()
+        assert 2 == self.cal.div(4, 2)
